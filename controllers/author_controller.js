6e9915c8a799f3a4f74fa88d842186885567fa27
Carlos Castillo <sir.ragnar@gmail.com>
2015-07-11 22:47:52 +0200

    Agregado enlace a perfil del autor y al proyecto en github

---
+++ controllers/author_controller.js
@@ -0,0 +1,9 @@
+// Controlador de autores
+
+// GET /author
+exports.profile = function(req,res){
+	res.render('author', 
+				{nombre: 'Carlos Castillo Alarc\u00F3n',
+				 portraitURL: '/images/author/portrait.png'}
+			);
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 // Declaración del controller de preguntas
 var quizController = require('../controllers/quiz_controller');
 
+// Declaración del controlador de página de autor
+var authorController = require('../controllers/author_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -12,5 +15,6 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/author', authorController.profile);
 
 module.exports = router;
