4268e4d0dd17dcdd8252b5c6b5f47e74cf365049
Carlos Castillo <sir.ragnar@gmail.com>
2015-08-02 21:12:24 +0200

    Cambio el uso de findById por find para evitar error en heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,7 +5,7 @@ var models=require('../models/models.js');
 
 // Autoload - factoriza el código si la ruta incluye :quizId
 exports.load=function(req,res,next,quizId){
-    models.Quiz.findById(quizId).then(
+    models.Quiz.find(quizId).then(
         function(quiz){
             if(quiz){
                 req.quiz=quiz;
