ad8343fc8edfe6f170fae00c27d6a5f347e06194
Carlos Castillo Alarcón <sir.ragnar+npm@gmail.com>
2015-08-20 02:48:34 +0200

    Quiz17 pasos 1 y 2: Crear middleware de autorización

--- routes/index.js
+++ routes/index.js
@@ -34,15 +34,15 @@ router.get('/logout', sessionController.destroy); // Destruir sesión
 // La lista de recursos es la primera del API y se resuelve en el controlador con index.
 router.get('/quizes',                      quizController.index);
 // Ruta para crear una pregunta nueva
-router.get('/quizes/new',                  quizController.new);
+router.get('/quizes/new',                  sessionController.loginRequired, quizController.new);
 // Ruta de respuesta tras crear la pregunta
-router.post('/quizes/create',              quizController.create);
+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
 // Ruta para iniciar la edición de una pregunta
-router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
 // Ruta que hace el put para actualizar una pregunta
-router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 // Ruta para eliminar una pregunta
-router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 // La ruta que muestra el recurso principal suele ser show en el controlador
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 // De esta cuelgan todas las que saquen información relacionada con el recurso
